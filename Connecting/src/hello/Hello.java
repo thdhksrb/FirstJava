e0cfc7523b10825972cf1de38478878eb17b89d2
swk <swk98@naver.com>
2023-03-09 22:48:24 +0900

    com3

--- Connecting/src/hello/Hello.java
+++ Connecting/src/hello/Hello.java
@@ -7,6 +7,10 @@ public class Hello
 	{
 		System.out.println("hello~~");
 		System.out.println("hello~~");
+		System.out.println("hello~~");
+		System.out.println("hello~~");
+		System.out.println("hello~~");
+		System.out.println("hello~~");
 	
 		
 		
